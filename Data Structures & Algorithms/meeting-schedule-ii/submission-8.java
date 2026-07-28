e9829acfc33a364bd40c73222f96113ffb6464d5
Tapas Biswas <39775386+tkbiswas@users.noreply.github.com>
2026-07-28 18:36:10 +0530

    Add: meeting-schedule-ii - submission-8

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-8.java
@@ -0,0 +1,25 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public int minMeetingRooms(List<Interval> intervals) {
+        intervals.sort((a,b) -> a.start - b.start);
+        var minHeap = new PriorityQueue<Integer>();
+        for(Interval interval : intervals){
+            if(!minHeap.isEmpty() && minHeap.peek() <= interval.start){
+                minHeap.poll();
+            }
+            minHeap.offer(interval.end);
+            
+        }
+        return minHeap.size();
+    }
+}
