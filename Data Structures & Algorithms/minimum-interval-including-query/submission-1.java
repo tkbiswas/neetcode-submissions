d43f839a2619c19bcc9ec1a1adf71f2bb1f56953
Tapas Biswas <39775386+tkbiswas@users.noreply.github.com>
2026-08-03 20:34:23 +0530

    Add: minimum-interval-including-query - submission-1

---
+++ Data Structures & Algorithms/minimum-interval-including-query/submission-1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] minInterval(int[][] intervals, int[] queries) {
+        Arrays.sort(intervals, (a,b) -> (a[1]-a[0] - (b[1] - b[0])));
+        var res = new int[queries.length];
+        for(int i=0; i< queries.length; i++){
+           for(int j=0; j< intervals.length; j++){
+                if(intervals[j][0] <= queries[i] && queries[i] <= intervals[j][1]){
+                    res[i] = intervals[j][1] - intervals[j][0] +1;
+                    break;
+                }
+                res[i] = -1;
+            }
+        }
+        return res;
+    }
+}
