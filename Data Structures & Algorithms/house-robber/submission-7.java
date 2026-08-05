35496ae706f8ec5d84b204dfac710b021431f660
Tapas Biswas <39775386+tkbiswas@users.noreply.github.com>
2026-08-05 09:23:32 +0530

    Add: house-robber - submission-7

---
+++ Data Structures & Algorithms/house-robber/submission-7.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int rob(int[] nums) {
+        if(nums.length == 0) return 0;
+        if(nums.length == 1) return nums[0];
+        int[] memo = new int[nums.length];
+        memo[0] = nums[0];
+        memo[1] = Math.max(nums[0], nums[1]);
+        for(int i =2; i< nums.length; i++){
+            memo[i] = Math.max(nums[i] + memo[i-2], memo[i-1]);
+        }
+        return memo[nums.length-1];
+    }
+}
